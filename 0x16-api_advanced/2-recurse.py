1fb41ed7f9ae327e2012fa7a0bf482ec6b40ce83
Gmukoro <gmukoro3@gmail.com>
2024-05-22 04:32:09 -0700

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,9 +6,7 @@ import requests
 def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
-    }
+    headers = {"User-Agent": "Python/requests"}
     params = {
         "after": after,
         "count": count,
