1c355dfd9845480fed3f852644a11f5884b37f30
Gmukoro <gmukoro3@gmail.com>
2024-05-22 08:59:50 -0700

    errors fixing

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,13 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Python/requests"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    
+    if response.status_code != 200:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    try:
+        data = response.json()
+        return data.get("data", {}).get("subscribers", 0)
+    except ValueError:
+        return 0
+
