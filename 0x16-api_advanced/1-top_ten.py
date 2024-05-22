ecafa32c09a58afe4fe1904d13371568b3a09f13
Gmukoro <gmukoro3@gmail.com>
2024-05-22 10:09:13 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,26 +6,26 @@ import requests
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(
-            url, headers=headers, params=params, allow_redirects=False
-            )
-
-    if response.status_code != 200:
-        print("None")
-        return
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    params = {"limit": 10}
 
     try:
-        results = response.json().get("data")
-        if results is None:
+        response = requests.get(
+                url, headers=headers, params=params, allow_redirects=False)
+
+        if response.status_code != 200:
             print("None")
             return
-        for child in results.get("children", []):
-            print(child.get("data", {}).get("title"))
-    except ValueError:
+
+        try:
+            results = response.json().get("data")
+            if results is None or not results.get("children"):
+                print("None")
+                return
+
+            for child in results.get("children", []):
+                print(child.get("data", {}).get("title"))
+        except ValueError:
+            print("None")
+    except requests.RequestException:
         print("None")
