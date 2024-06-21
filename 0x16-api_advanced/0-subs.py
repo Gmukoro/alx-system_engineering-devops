132fda9cba5a79d5b53fcd70a211ff7e0f11ae5d
Gmukoro <gmukoro3@gmail.com>
2024-06-20 17:39:58 -0700

    0-subs'.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "MyRedditApp/1.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
@@ -17,4 +17,3 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
-
